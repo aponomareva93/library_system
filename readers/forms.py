391710faec7ac1f4125a634209e94a5546505951
aponomareva93 <apon1993@gmail.com>
2015-11-05 23:36:34 +0600

    Were created a template for login/logout/changing password for readers (not only for ordinary 'User-model' users) and added appropriate html files

---
+++ readers/forms.py
@@ -0,0 +1,22 @@
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+    template_name = "login.html"
+    success_url = "/homepage/"
+
+    def form_valid(self, form):
+        self.user = form.get_user()
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect("/index/")
